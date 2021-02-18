b63164a0e099674e4ea441611ddfec78cf732270
SuperTapood <yoav.o252@gmail.com>
2021-02-18 20:52:10 +0200

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import com.mlomb.freetypejni.FreeType;
+import com.mlomb.freetypejni.Library;
+
 import potato.nightly.Window;
 
 public class Main {
 
     public static void main(String[] args) {
+        Library library = FreeType.newLibrary();
+        System.out.println("Version " + library.getVersion());
         Window window = new Window();
         window.init();
         window.run();
